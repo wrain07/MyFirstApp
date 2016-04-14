042fcf5946806893e3ca6faf9db1235c23720fdd
Jennifer P. Gee <winterrain107@yahoo.com>
2016-04-14 09:40:22 -0400

    Refactor

---
+++ src/main/java/firstapp/TestController.java
@@ -0,0 +1,13 @@
+package firstapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello World First App";
+	}
+}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World in development April 15</h2>
-</body>
-</html>
